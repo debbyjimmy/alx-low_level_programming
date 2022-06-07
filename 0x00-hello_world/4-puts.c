e4974c32b961862bc371d22e6678612a4c8e5686
debbyjimmy <debbyjim90@gmail.com>
2022-06-06 22:00:00 -0700

    edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-/* main - Entry point returns  zero */
+/* main - Entry point */
+/* returns  zero */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
